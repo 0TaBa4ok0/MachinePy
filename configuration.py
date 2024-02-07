2a7dd3a748890ce08b1984f0ce44eba74be7a22f
Dmitry <157880580+0TaBa4ok0@users.noreply.github.com>
2024-02-07 14:33:19 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = " https://ee5cdc5d-f1a6-43fe-8646-f61ac4169c47.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
